4c485bb5c218040c6f2555d83ac6a515ee9d5858
Rob McBroom <github@skurfer.com>
2018-01-31 23:37:57 -0500

    add event triggers for adding/removing devices

--- Audio/AudioSource.m
+++ Audio/AudioSource.m
@@ -11,7 +11,26 @@
 OSStatus QSAudioDeviceListener(AudioObjectID inObjectID, UInt32 inNumberAddresses, const AudioObjectPropertyAddress inAddresses[], void* inClientData)
 {
 	QSCatalogEntry *audioEntry = [QSLib entryForID:@"QSAudioDevicesPreset"];
+	NSMutableSet *before = [NSMutableSet setWithArray:[audioEntry contents]];
 	[audioEntry scanForced:YES];
+	NSDate *giveUp = [NSDate dateWithTimeIntervalSinceNow:5.0];
+	while ([audioEntry isScanning] && [[NSDate date] isLessThan:giveUp]) {
+		// wait up to 5 seconds for the rescan to complete
+		sleep(0.1);
+	}
+	NSMutableSet *after = [NSMutableSet setWithArray:[audioEntry contents]];
+	NSDictionary *info;
+	if ([after count] > [before count]) {
+		// device added
+		[after minusSet:before];
+		info = @{@"object": [after anyObject]};
+		[[NSNotificationCenter defaultCenter] postNotificationName:@"QSEventNotification" object:@"QSAudioDeviceAddedEvent" userInfo:info];
+	} else {
+		// device removed
+		[before minusSet:after];
+		info = @{@"object": [before anyObject]};
+		[[NSNotificationCenter defaultCenter] postNotificationName:@"QSEventNotification" object:@"QSAudioDeviceRemovedEvent" userInfo:info];
+	}
 	return noErr;
 }
 
