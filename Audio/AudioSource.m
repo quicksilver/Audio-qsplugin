61bcd60ce82950fd77b1ddf4415816a1f0692e05
Rob McBroom <github@skurfer.com>
2018-02-01 18:28:30 -0500

    move event triggers to a dedicated provider
    using the add/remove methods allows us to only listen when needed

--- Audio/AudioSource.m
+++ Audio/AudioSource.m
@@ -8,58 +8,7 @@
 #import "AudioSource.h"
 #import "QSAudio.h"
 
-OSStatus QSAudioDeviceListener(AudioObjectID inObjectID, UInt32 inNumberAddresses, const AudioObjectPropertyAddress inAddresses[], void* inClientData)
-{
-	QSCatalogEntry *audioEntry = [QSLib entryForID:@"QSAudioDevicesPreset"];
-	NSMutableSet *before = [NSMutableSet setWithArray:[audioEntry contents]];
-	[audioEntry scanForced:YES];
-	NSDate *giveUp = [NSDate dateWithTimeIntervalSinceNow:5.0];
-	while ([audioEntry isScanning] && [[NSDate date] isLessThan:giveUp]) {
-		// wait up to 5 seconds for the rescan to complete
-		sleep(0.1);
-	}
-	NSMutableSet *after = [NSMutableSet setWithArray:[audioEntry contents]];
-	NSDictionary *info;
-	if ([after count] > [before count]) {
-		// device added
-		[after minusSet:before];
-		info = @{@"object": [after anyObject]};
-		[[NSNotificationCenter defaultCenter] postNotificationName:@"QSEventNotification" object:@"QSAudioDeviceAddedEvent" userInfo:info];
-	} else {
-		// device removed
-		[before minusSet:after];
-		info = @{@"object": [before anyObject]};
-		[[NSNotificationCenter defaultCenter] postNotificationName:@"QSEventNotification" object:@"QSAudioDeviceRemovedEvent" userInfo:info];
-	}
-	return noErr;
-}
-
 @implementation QSAudioSource
-
-- (instancetype)init
-{
-	self = [super init];
-	if (self) {
-		AudioObjectPropertyAddress propertyAddress = {
-			kAudioHardwarePropertyDevices,
-			kAudioObjectPropertyScopeGlobal,
-			kAudioObjectPropertyElementMaster
-		};
-		AudioObjectAddPropertyListener(kAudioObjectSystemObject, &propertyAddress, QSAudioDeviceListener, NULL);
-	}
-	return self;
-}
-
-- (void)dealloc
-{
-	AudioObjectPropertyAddress propertyAddress = {
-		kAudioHardwarePropertyDevices,
-		kAudioObjectPropertyScopeGlobal,
-		kAudioObjectPropertyElementMaster
-	};
-	AudioObjectRemovePropertyListener(kAudioObjectSystemObject, &propertyAddress, QSAudioDeviceListener, NULL);
-	[super dealloc];
-}
 
 #pragma mark Catalog Entry Methods
 
---
+++ Audio/QSAudioEvents.h
@@ -0,0 +1,9 @@
+//
+//  QSAudioEvents.h
+//  Audio Plugin
+//
+//  Created by Rob McBroom on 2018/02/01.
+//
+
+@interface QSAudioEvents : NSObject <QSEventTriggerProvider>
+@end
